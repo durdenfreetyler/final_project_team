c7ea107b0343a3a4c955ac016162a8ead8336b9a
andrii-grynik <andrii.grynik@gmail.com>
2023-03-20 09:43:05 -0600

    "ini"

---
+++ backend/knexfile.js
@@ -0,0 +1,57 @@
+// Update with your config settings.  (__dirname) replaced in seed and migrat
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'labber',
+      password : 'labber',
+      database : 'final',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: final + '/knex/migrations',
+    },
+    seeds: {
+      directory: final + '/knex/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
