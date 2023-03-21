b289f7a0fc3a08973a2bc66805b58dc70b6b7498
andrii-grynik <andrii.grynik@gmail.com>
2023-03-21 17:13:39 -0600

    "JOIN tables"

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-
+// getting to users data table
 app.get("/users", (req, res) => {
   knex.select()
   .from('users')
@@ -19,6 +19,7 @@ app.get("/users", (req, res) => {
     res.json(result);
   })  
 });
+// getting to challenge data table
 app.get("/challenge", (req, res) => {
   knex.select()
   .from('challenge')
@@ -26,12 +27,43 @@ app.get("/challenge", (req, res) => {
     res.json(result);
   })  
 });
-
+// getting to user_challenge data table  
 app.get("/user_challenge", (req, res) => {
   knex.select()
   .from('user_challenge')
   .then((result) =>{
     res.json(result);
+  })  
+});
+// Join statement between User, User_Challenge, and Challenge tables
+app.get("/u-c-uc", (req, res) => {
+  knex.select('*')
+  .from('users')
+  .leftJoin('user_challenge', 'user_challenge.user_id','users.id')
+  .leftJoin('challenge', 'user_challenge.challenge_id', 'challenge.id')
+  .then((result) =>{
+    res.send(result);
+  })  
+});
+
+// Join statement between User and User_Challenge tables
+app.get("/u-uc", (req, res) => {
+  knex.select('*')
+  .from('users')
+  .leftJoin('challenge', 'challenge.created_by','users.id')
+  .then((result) =>{
+    res.send(result);
+  })  
+});
+
+// Join statement between User_Challenge, and Challenge tables
+app.get("/uc-c", (req, res) => {
+  knex.select('*')
+  .from('user_challenge')
+  //.leftJoin('user_challenge', 'user_challenge.user_id','users.id')
+  .leftJoin('challenge', 'user_challenge.challenge_id', 'challenge.id')
+  .then((result) =>{
+    res.send(result);
   })  
 });
 
