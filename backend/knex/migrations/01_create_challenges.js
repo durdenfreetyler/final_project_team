d7b651a7b26f3e849a0e3dc644d1d6be353f9dc4
Abe <abrahamrodgers@gmail.com>
2023-03-21 10:45:42 -0700

    Split migrations up and test

---
+++ backend/knex/migrations/01_create_challenges.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function (knex) {
+  return knex.schema.createTable("challenge", function (table) {
+    table.increments("id");
+    table.string("title", 255).notNullable();
+    table.text("description").notNullable();
+    table.integer("created_by").unsigned().references("id").inTable("users");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable("challenge")
+};
---
+++ backend/knex/migrations/02_create_user_challenges_.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+
+    .createTable("user_challenge", function (table) {
+      table.increments("id");
+      table.integer("user_id").unsigned().references("id").inTable("users");
+      table
+        .integer("challenge_id")
+        .unsigned()
+        .references("id")
+        .inTable("challenge");
+      table.string("criteria_type", 255).notNullable();
+      table.string("criteria_value", 255).notNullable();
+      table.integer("progress").defaultTo(0);
+      table.unique(["user_id", "challenge_id"]);
+    });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable("user_challenge")  
+};
