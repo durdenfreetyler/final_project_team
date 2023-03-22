edad55a4adc578880f6b1cf56cb7ca395378eecc
jackymomomo <jacklloydrobinson@gmail.com>
2023-03-21 20:19:42 -0700

    bad

--- frontend/src/components/Login/challange_creator/ChallangeCreator.js
+++ frontend/src/components/Login/challange_creator/ChallangeCreator.js
@@ -1,57 +1,82 @@
-import React, { useState } from 'react';
-import ChallengeCreator from './ChallangeCreator';
+import { useState } from "react";
 
-function createChallenge() {
-  const [difficulty, setDifficulty] = useState('');
-  const [timeLimit, setTimeLimit] = useState('');
-  const [rewardPoints, setRewardPoints] = useState('');
+export default function ChallengeForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [difficulty, setDifficulty] = useState("easy");
+  const [timeLimit, setTimeLimit] = useState(0);
+  const [rewardPoints, setRewardPoints] = useState(0);
 
-  const handleSubmit = async (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
-    try {
-      const response = await fetch('/api/create-challenge', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ difficulty, timeLimit, rewardPoints })
+
+    const challengeData = {
+      title,
+      description,
+      difficulty,
+      time_limit: timeLimit,
+      reward_points: rewardPoints,
+    };
+
+    // Send the challenge data to the server using a fetch request
+    fetch("/challenge", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(challengeData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Challenge created successfully:", data);
+      })
+      .catch((error) => {
+        console.error("Error creating challenge:", error);
       });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="difficulty">Difficulty:</label>
-      <input
-        type="text"
-        id="difficulty"
-        value={difficulty}
-        onChange={(event) => setDifficulty(event.target.value)}
-      />
-
-      <label htmlFor="timeLimit">Time Limit:</label>
-      <input
-        type="text"
-        id="timeLimit"
-        value={timeLimit}
-        onChange={(event) => setTimeLimit(event.target.value)}
-      />
-
-      <label htmlFor="rewardPoints">Reward Points:</label>
-      <input
-        type="text"
-        id="rewardPoints"
-        value={rewardPoints}
-        onChange={(event) => setRewardPoints(event.target.value)}
-      />
-
+      <label>
+        Title:
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </label>
+      <label>
+        Description:
+        <textarea
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </label>
+      <label>
+        Difficulty:
+        <select value={difficulty} onChange={(event) => setDifficulty(event.target.value)}>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </label>
+      <label>
+        Time Limit (in seconds):
+        <input
+          type="number"
+          value={timeLimit}
+          onChange={(event) => setTimeLimit(event.target.value)}
+        />
+      </label>
+      <label>
+        Reward Points:
+        <input
+          type="number"
+          value={rewardPoints}
+          onChange={(event) => setRewardPoints(event.target.value)}
+        />
+      </label>
       <button type="submit">Create Challenge</button>
     </form>
   );
 }
-
-export default ChallengeCreator;
