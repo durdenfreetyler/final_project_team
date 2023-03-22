dbea6cf5d208d7c12ba0d0e560ec44f9677e1dc7
Abe <abrahamrodgers@gmail.com>
2023-03-21 19:41:34 -0700

    Passport installed

---
+++ frontend/contexts/AuthContext.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
