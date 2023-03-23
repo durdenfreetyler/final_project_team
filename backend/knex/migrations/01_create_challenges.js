49a64c55ec4b1750ddcc9d8c3f394ca79570efaa
andrii-grynik <andrii.grynik@gmail.com>
2023-03-23 10:15:58 -0600

    "Date/time table"

--- backend/knex/migrations/01_create_challenges.js
+++ backend/knex/migrations/01_create_challenges.js
@@ -8,6 +8,7 @@ exports.up = function (knex) {
     table.increments("id");
     table.string("title", 255).notNullable();
     table.text("description").notNullable();
+    table.date("date").defaultTo(knex.fn.now());
     table.integer("created_by").unsigned().references("id").inTable("users");
   });
 };
