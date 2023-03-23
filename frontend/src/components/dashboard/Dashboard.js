cefe2dd97fcc07acb37f178c6a266f90c513e535
Abe <abrahamrodgers@gmail.com>
2023-03-22 19:16:29 -0700

    Co-authored-by: jackymomomo <jackymomomo@users.noreply.github.com> Co-authored-by: Andrii  <andrii-grynik@users.noreply.github.com>

---
+++ frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,6 @@
+//
+// create challenge (challenge captain) need a boolean for captain selection
+// show all past and current challenges / (we need to set it to date for past and current)
+// show form that allows to update progress to a current challenge (boolean)
+// point counter ( end of the day chellenge point counter ) sum function for point value (looking only for true bools)
+//
