d2d2233b27e1f76e2b30b3baf80a0ce976299d8b
jackymomomo <jacklloydrobinson@gmail.com>
2023-03-25 15:38:29 -0700

    styling for front page lists challanges in grid

--- frontend/src/components/front-page/FrontPage.js
+++ frontend/src/components/front-page/FrontPage.js
@@ -6,26 +6,53 @@ import Card from './cards';
 import Leaderboard from './LeaderBoard'
 import Slideshow from './SlideShow';
 import SignupSlideshow from './SlideShow';
+import { Helmet } from 'react-helmet';
+
+
+
 //import Leader_Button from './LeaderBoard_Button'
 
-
+<Helmet>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  </Helmet>
+  
 function HomePage() {
+  
   return (
+    
     <div className="App">
        <Background></Background>
       <header className="App-header">
-      <nav class="navMenu">
-      <a href="/">Home</a>
-      <a href="/dashboard">Account</a>
-      <a href="/create-challenge">Create</a>
-      <a href="#">About</a>
-      <div class="dot"></div>
+      <nav class="nav-Bar">
+      <a href="#" class="logo">Challenge Champions</a>
+      <div class="nav-links">
+        <div class="social-buttons">
+          <a href="https://www.facebook.com/profile.php?id=100089970687570" class="social-button social-button--facebook" aria-label="Facebook">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.instagram.com/wok_and_roll98/" class="social-button social-button--instagram" aria-label="Instagram">
+            <i class="fa-brands fa-instagram"></i>          </a>
+          <a href="#" class="social-button social-button--snapchat" aria-label="Snapchat">
+            <i class="fab fa-snapchat-ghost"></i>
+          </a>
+          <a href="https://github.com/jackymomomo/Food-Pick-up-Ordering-app" class="social-button social-button--github" aria-label="GitHub">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="#" class="social-button social-button--codepen" aria-label="CodePen">
+            <i class="fab fa-codepen"></i>
+          </a>
+        </div>
+
+      </div>
+      <a href="/login" id="cart-icon" class="signup-btn">LOGIN<div id="cart-icon">
+        <i class="fa-solid fa-bag-shopping fa-lg"></i>
+      </div></a>
     </nav>
     <div class="overlay">
 <h1>Challenge Champions</h1>
 <h3></h3>
 	<br></br>
-	<a href='/login' className='button'> login </a>
 		</div>
     {/* <Leader_Button></Leader_Button> */}
       </header>
@@ -81,31 +108,40 @@ function HomePage() {
   <Slideshow></Slideshow>
     </div>
     <footer>
-  <div class="container">
-    <div class="banana">
-      <div class="col-md-4">
-        <h3>About Us</h3>
-        <p>We are a team of web developers who are passionate about creating beautiful and functional websites.</p>
+      <div class="footer-container">
+        <div class="footer-logo">
+          <img src="https://i.pinimg.com/564x/db/63/df/db63dfb015cec41306deee05c762d2a0.jpg" alt="Logo"></img>
+          <h3>Wok & Roll</h3>
+        </div>
+        
+        <div class="footer-social">
+          <h3>Follow us on social media</h3>
+          <div class="social-icons">
+            <a href="https://www.facebook.com/profile.php?id=100089970687570"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/wok_and_roll98/"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+
+        <div class="footer-nav">
+          <div class="3d-menu">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Service</a></li>
+              <li><a href="#">Play list</a></li>
+              <li><a href="#">Subscribe</a></li>
+              <li><a href="#">Like</a></li>
+            </ul>
+          </div>
+        </div>
+
       </div>
-      <div class="col-md-4">
-        <h3>Contact Us</h3>
-        <ul class="contact-list">
-          <li><i class="fa fa-map-marker"></i> 123 Main Street, Anytown, USA</li>
-          <li><i class="fa fa-phone"></i> (555) 555-5555</li>
-          <li><i class="fa fa-envelope"></i> info@yourwebsite.com</li>
-        </ul>
+      <div class="footer-bottom">
+        <p>&copy; 2023 Wok & Roll. All rights reserved.</p>
       </div>
-      <div class="col-md-4">
-        <h3>Follow Us</h3>
-        <ul class="social-list">
-          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</footer>
+    </footer>
 
     </div>
   );
