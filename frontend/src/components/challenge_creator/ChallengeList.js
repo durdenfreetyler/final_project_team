01b4e20ebc16baee78c2920150dbb4eb88762504
andrii-grynik <andrii.grynik@gmail.com>
2023-03-26 20:36:38 -0600

    "ChellengeList update for mark complete"

--- frontend/src/components/challenge_creator/ChallengeList.js
+++ frontend/src/components/challenge_creator/ChallengeList.js
@@ -1,12 +1,74 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProgressCard from "../ProgressBar/ProgressCard";
+import { JoinChallenge } from "./JoinChallenge";
+
 
 function ChallengeList(props) {
   const { userId } = props;
   const [currentChallenges, setCurrentChallenges] = useState([]);
   const [expiredChallenges, setExpiredChallenges] = useState([]);
   
+
+  const handleChallengeCheckIn = async (challengeId) => {
+    try {
+      const response = await axios.put(
+        `http://localhost:3001/user_challenge/${challengeId}`,
+        { is_completed: true },
+        { withCredentials: true }
+      );
+      const updatedChallenge = response.data.data;
+      // Remove the updated challenge from the currentChallenges array
+      setCurrentChallenges((prevChallenges) =>
+        prevChallenges.filter((challenge) => challenge.id !== challengeId)
+      );
+
+      // Add the updated challenge to the expiredChallenges array
+      setExpiredChallenges((prevChallenges) => [
+        ...prevChallenges,
+        {
+          ...currentChallenges.find(
+            (challenge) => challenge.id === challengeId
+          ),
+          is_completed: true,
+        },
+      ]);
+
+      console.log("response", response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  /*
+  const handleJoinChallenge = (newChallenge) => {
+    setCurrentChallenges((prevChallenges) => [...prevChallenges, newChallenge]);
+  };
+
+  const fetchUserChallenges = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3001/user_challenge`, {
+        withCredentials: true,
+      });
+      const challenges = response.data;
+      const currentDate = new Date().getTime();
+      const expired = challenges.filter(
+        (challenge) =>
+          currentDate >=
+          new Date(challenge.expiration_date.toString()).getTime()
+      );
+      const active = challenges.filter(
+        (challenge) =>
+          currentDate < new Date(challenge.expiration_date.toString()).getTime()
+      );
+      setCurrentChallenges(active);
+      setExpiredChallenges(expired);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+*/ 
+// I am not a 100% sure this code is needed  here we have the same code in AvailableChallenges
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,6 +101,7 @@ function ChallengeList(props) {
         console.error(error.message);
       }
     };
+    
     fetchData();
   }, [userId]);
   //console.log(expiredChallenges);
@@ -85,6 +148,9 @@ function ChallengeList(props) {
           <p>Points: {challenge.points}</p>
           <p>Expiration Date: {challenge.expiration_date}</p>
           <button onClick={() => handleDelete(challenge.challenge_id)}>Delete</button>
+          <button onClick={() => handleChallengeCheckIn(challenge.id)}>
+            Mark Completed
+          </button>
         </div>
       ))}
       <h2>Expired Challenges</h2>
@@ -95,6 +161,17 @@ function ChallengeList(props) {
           <p>Points: {challenge.points}</p>
           <p>Expiration Date: {challenge.expiration_date}</p>
           <button onClick={() => handleDelete(challenge.challenge_id)}>Delete</button>
+          {challenge.completed_before_expiration ? (
+            <p>
+              Player completed challenge before expiration and did not to
+              charity
+            </p>
+          ) : (
+            <p>
+              Player did not complete challenge before expiration and donated to
+              charity
+            </p>
+            )}  
         </div>
       ))}
     </div>
